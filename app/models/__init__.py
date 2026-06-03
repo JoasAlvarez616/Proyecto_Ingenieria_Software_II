d943e8e524d459020a19da32adec40e096cd71c0
JandI616 <alvarezcastrillon757@gmail.com>
2026-06-03 09:52:01 -0500

    Limpieza de estructura, carpetas duplicadas eliminadas

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.room import Room
-from app.models.client import Client
-from app.models.reservation import Reservation
-from app.models.payment import Payment
-from app.models.user import User
-
-__all__ = ["Room", "Client", "Reservation", "Payment", "User"]

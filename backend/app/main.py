38839dfad54a397c0ae442992eaaa4f02f91b290
Jesus Lopez <jesulopfer21@gmail.com>
2026-06-22 11:20:51 -0500

    Allow all CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -100,10 +100,9 @@ os.makedirs("static", exist_ok=True)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Configurar CORS dinámico para permitir entornos de producción
-cors_origins = os.getenv("CORS_ORIGINS", "http://localhost:5173").split(",")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[origin.strip() for origin in cors_origins],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
